12de9291d6d13723bde3f03916666ae8ed3d3e28
valeriadmitrievna1506dev <valeriakluch.dev@gmail.com>
2021-03-01 15:08:53 +0300

    combine routes in one file

---
+++ server/routes.js
@@ -0,0 +1,27 @@
+module.exports = (app) => {
+  app.set('strict routing', true);
+  app.get('/', (req, res) =>
+    res.status(200).send({
+      message: 'Welcome to the Todo-APP',
+    })
+  );
+
+  const klawSync = require('klaw-sync');
+  const path = require('path');
+
+  async function useControllers() {
+    const paths = klawSync(path.resolve('server/controllers'), {
+      nodir: true,
+    });
+    paths.forEach((file) => {
+      if (
+        path.basename(file.path)[0] === '_' ||
+        path.basename(file.path)[0] === '.'
+      )
+        return;
+      app.use('/', require(file.path));
+    });
+  }
+
+  useControllers();
+};
