7164b1697d17c9ffc9c6750d2bdae05d36596857
valeriadmitrievna1506dev <valeriakluch.dev@gmail.com>
2021-03-10 11:02:11 +0300

    Last fixes for heroku (loading and other)

--- app.js
+++ app.js
@@ -4,37 +4,32 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sequelize = require('./database');
-const path = require('path')
-const models = require('./server/models/index')
+const path = require('path');
+const models = require('./server/models/index');
 const PORT = process.env.PORT || 8000;
-const errorHandler = require('./server/middleware/ErrorHandler')
+const errorHandler = require('./server/middleware/ErrorHandler');
 
 const app = express();
 
-// const static = __dirname + '/views/'
+const static = __dirname + '/client/build/';
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(express.static(static))
-
-// app.get('/', function (req, res) {
-//   res.sendFile(static + "index.html");
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
+    app.use(express.static(static));
     require('./server/routes')(app);
 
-    app.use(errorHandler)
-    
+    app.use(errorHandler);
+
     app.listen(PORT, () => console.log(`server running on ${PORT}`));
   } catch (e) {
     console.log(e.message);
   }
 };
-start()
+start();
